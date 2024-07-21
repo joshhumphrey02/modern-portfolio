2ec1b3347d182e1fc0874a02846adbe660252d57
Humphrey Joshua <85317065+joshhumphrey02@users.noreply.github.com>
2024-07-21 17:22:49 +0100

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+
+import './globals.css';
+import { ThemeProvider } from './provider';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+	title: "Humphrey's Portfolio",
+	description: 'Everything about my works and experiences',
+};
+
+export default function RootLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode;
+}>) {
+	return (
+		<html lang="en" suppressHydrationWarning>
+			<head>
+				<link rel="icon" href="/logo.svg" sizes="any" />
+			</head>
+			<body className={inter.className}>
+				<ThemeProvider
+					attribute="class"
+					defaultTheme="dark"
+					enableSystem
+					disableTransitionOnChange>
+					{children}
+				</ThemeProvider>
+			</body>
+		</html>
+	);
+}
---
+++ data/index.ts
@@ -0,0 +1,217 @@
+export const navItems = [
+	{ name: 'About', link: '#about' },
+	{ name: 'Projects', link: '#projects' },
+	{ name: 'Testimonials', link: '#testimonials' },
+	{ name: 'Contact', link: '#contact' },
+];
+
+export const gridItems = [
+	{
+		id: 1,
+		title: 'I prioritize client collaboration, fostering open communication ',
+		description: '',
+		className: 'lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]',
+		imgClassName: 'w-full h-full',
+		titleClassName: 'justify-end',
+		img: '/b1.svg',
+		spareImg: '',
+	},
+	{
+		id: 2,
+		title: "I'm very flexible with time zone communications",
+		description: '',
+		className: 'lg:col-span-2 md:col-span-3 md:row-span-2',
+		imgClassName: '',
+		titleClassName: 'justify-start',
+		img: '',
+		spareImg: '',
+	},
+	{
+		id: 3,
+		title: 'My tech stack',
+		description: 'I constantly try to improve',
+		className: 'lg:col-span-2 md:col-span-3 md:row-span-2',
+		imgClassName: '',
+		titleClassName: 'justify-center',
+		img: '',
+		spareImg: '',
+	},
+	{
+		id: 4,
+		title: 'Tech enthusiast with a passion for development.',
+		description: '',
+		className: 'lg:col-span-2 md:col-span-3 md:row-span-1',
+		imgClassName: '',
+		titleClassName: 'justify-start',
+		img: '/grid.svg',
+		spareImg: '/b4.svg',
+	},
+
+	{
+		id: 5,
+		title: 'Currently building a Service Attendance Web App',
+		description: 'The Inside Scoop',
+		className: 'md:col-span-3 md:row-span-2',
+		imgClassName: 'absolute right-0 bottom-0 md:w-96 w-60',
+		titleClassName: 'justify-center md:justify-start lg:justify-center',
+		img: '/b5.svg',
+		spareImg: '/grid.svg',
+	},
+	{
+		id: 6,
+		title: 'Do you want to start a project together?',
+		description: '',
+		className: 'lg:col-span-2 md:col-span-3 md:row-span-1',
+		imgClassName: '',
+		titleClassName: 'justify-center md:max-w-full max-w-60 text-center',
+		img: '',
+		spareImg: '',
+	},
+];
+
+export const projects = [
+	{
+		id: 1,
+		title: 'An Electronic Ecommerce Admin',
+		des: 'An admin software with modern features built with the latest tech stack.',
+		img: '/p1.png',
+		iconLists: ['/re.svg', '/next.svg', '/tail.svg', '/ts.svg'],
+		link: 'https://seller-anchorage.web.app',
+	},
+	{
+		id: 2,
+		title: 'A simple personal website',
+		des: 'Simplify your mountain experience with your friends.',
+		img: '/p3.png',
+		iconLists: ['/next.svg', '/tail.svg', '/ts.svg'],
+		link: 'https://hikeking.web.app',
+	},
+	// {
+	// 	id: 3,
+	// 	title: 'AI Image SaaS - Canva Application',
+	// 	des: 'A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.',
+	// 	img: '/p3.png',
+	// 	iconLists: ['/re.svg', '/tail.svg', '/ts.svg', '/three.svg', '/c.svg'],
+	// 	link: '/ui.aiimg.com',
+	// },
+	// {
+	// 	id: 4,
+	// 	title: 'Animated Apple Iphone 3D Website',
+	// 	des: 'Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..',
+	// 	img: '/p4.svg',
+	// 	iconLists: ['/next.svg', '/tail.svg', '/ts.svg', '/three.svg', '/gsap.svg'],
+	// 	link: '/ui.apple.com',
+	// },
+];
+
+export const testimonials = [
+	{
+		quote:
+			"Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+		name: 'Michael Johnson',
+		title: 'Director of AlphaStream Technologies',
+	},
+	{
+		quote:
+			"Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+		name: 'Michael Johnson',
+		title: 'Director of AlphaStream Technologies',
+	},
+	{
+		quote:
+			"Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+		name: 'Michael Johnson',
+		title: 'Director of AlphaStream Technologies',
+	},
+	{
+		quote:
+			"Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+		name: 'Michael Johnson',
+		title: 'Director of AlphaStream Technologies',
+	},
+	{
+		quote:
+			"Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+		name: 'Michael Johnson',
+		title: 'Director of AlphaStream Technologies',
+	},
+];
+
+export const companies = [
+	{
+		id: 1,
+		name: 'cloudinary',
+		img: '/cloud.svg',
+		nameImg: '/cloudName.svg',
+	},
+	{
+		id: 2,
+		name: 'appwrite',
+		img: '/app.svg',
+		nameImg: '/appName.svg',
+	},
+	{
+		id: 3,
+		name: 'HOSTINGER',
+		img: '/host.svg',
+		nameImg: '/hostName.svg',
+	},
+	{
+		id: 4,
+		name: 'stream',
+		img: '/s.svg',
+		nameImg: '/streamName.svg',
+	},
+	{
+		id: 5,
+		name: 'docker.',
+		img: '/dock.svg',
+		nameImg: '/dockerName.svg',
+	},
+];
+
+export const workExperience = [
+	{
+		id: 1,
+		title: 'Frontend Engineer',
+		desc: 'Assisted in the development of a web-based platform using React.js, enhancing interactivity.',
+		className: 'md:col-span-2',
+		thumbnail: '/exp1.svg',
+	},
+	{
+		id: 2,
+		title: 'Mobile App Dev - MyNebor Technologies',
+		desc: 'Designed and developed mobile app for both iOS & Android platforms using React Native.',
+		className: 'md:col-span-2', // change to md:col-span-2
+		thumbnail: '/exp2.svg',
+	},
+	{
+		id: 3,
+		title: 'Freelance Frontend Developer Project',
+		desc: 'Designed and built a modern React JS Admin Dashboard, from initial concept to deployment on app stores.',
+		className: 'md:col-span-2', // change to md:col-span-2
+		thumbnail: '/exp3.svg',
+	},
+	{
+		id: 4,
+		title: 'Lead Frontend Developer',
+		desc: 'Developed and maintained user-interative features using modern frontend technologies.',
+		className: 'md:col-span-2',
+		thumbnail: '/exp4.svg',
+	},
+];
+
+export const socialMedia = [
+	{
+		id: 1,
+		img: '/git.svg',
+	},
+	{
+		id: 2,
+		img: '/twit.svg',
+	},
+	{
+		id: 3,
+		img: '/link.svg',
+	},
+];
